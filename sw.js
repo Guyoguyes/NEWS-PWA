fc3db07157822de3f4c27438672d3c6247fa4151
Muhammad Feisal Shibiby <37222375+Shibiby@users.noreply.github.com>
2019-09-27 13:43:57 +0300

    commit

--- sw.js
+++ sw.js
@@ -1,52 +1,46 @@
 const staticAssets = [
-    './',
-    './app.js',
-    './style.css',
-    './node_modules/bootstrap/dist/css/bootstrap.min.css',
-    './node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css',
-    './node_modules/jquery/dist/jquery.min.js',
-    './node_modules/bootstrap/dist/js/bootstrap.min.js',
-    './node_modules/@fortawesome/fontawesome-free/js/fontawesome.min.js',
-    './fallback.json',
-    './images/fetch-dog.jpg'
+  './',
+  './app.js',
+  './style.css',
+  './node_modules/bootstrap/dist/css/bootstrap.min.css',
+  './node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css',
+  './node_modules/jquery/dist/jquery.min.js',
+  './node_modules/bootstrap/dist/js/bootstrap.min.js',
+  './node_modules/@fortawesome/fontawesome-free/js/fontawesome.min.js',
+  './fallback.json',
+  './images/fetch-dog.jpg'
 ]
 
-self.addEventListener('install', async e => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
-    return self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', e => {
-    self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', async e => {
-    const req = e.request;
-    const url = new URL(req.url);
-  
-    if (url.origin === location.origin) {
-      e.respondWith(cacheFirst(req));
-    } else {
-      e.respondWith(networkAndCache(req));
-    }
-  });
-  
-  async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cached = await cache.match(req);
-    return cached || fetch(req);
+self.addEventListener('install', async event => {
+  const cache = await caches.open('news-static');
+  cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  if(url.origin === location.origin){
+      event.respondWith(cacheFirst(req));
+  }else{
+      event.respondWith(networkFirst(req));
   }
-  
-  async function networkAndCache(req) {
-    const cache = await caches.open(cacheName);
-    try {
-      const fresh = await fetch(req);
-      await cache.put(req, fresh.clone());
-      return fresh;
-    } catch (e) {
-      const cached = await cache.match(req);
-      return cached;
-    }
+});
+
+async function cacheFirst(req){
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req){
+  const cache = await caches.open('news-dynamic');
+
+  try {
+      const res = await fetch(req);
+      cache.put(req, res.clone());
+      return res;
+  }catch (error){
+      const cachedResponse = await cache.match(req);
+      return cachedResponse || await caches.match('./fallback.json');
   }
-     
+}
